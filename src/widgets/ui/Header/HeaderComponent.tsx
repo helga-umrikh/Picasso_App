8259674c15b6fd4e882bd7b76f5204532454cef0
helga-umrikh <zetaumr@gmail.com>
2024-03-23 18:32:45 +0300

    feat: add header

--- src/widgets/ui/Header/HeaderComponent.tsx
+++ src/widgets/ui/Header/HeaderComponent.tsx
@@ -1,14 +1,17 @@
 import React from 'react'
-import {  Layout  } from 'antd';
-const { Header } = Layout;
+import { Layout } from 'antd'
+const { Header } = Layout
 
+const headerStyle = {
+  color: '#fff',
+  height: 64,
+  paddingInline: 48,
+  lineHeight: '64px',
+  backgroundColor: '#4096ff',
+};
 
 const HeaderComponent = () => {
-  return (
-    <Header>
-
-    </Header>
-  )
+    return <Header style={headerStyle}>Picasso App</Header>
 }
 
 export default HeaderComponent
