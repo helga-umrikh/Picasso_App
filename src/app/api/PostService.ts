f8ee37bb6894bdaab7346e74d3688f5b4ca5082d
helga-umrikh <zetaumr@gmail.com>
2024-03-23 19:28:20 +0300

    feat: add redux and api

---
+++ src/app/api/PostService.ts
@@ -0,0 +1,24 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { IPost } from '../interface/IPost'
+
+
+export const postAPI = createApi({
+    reducerPath: 'postAPI',
+    baseQuery: fetchBaseQuery({
+        baseUrl: 'https://jsonplaceholder.typicode.com/',
+    }),
+    endpoints: (build) => ({
+        fetchAllPosts: build.query<IPost[], {limit:number,start:number}>({
+            query: ({limit = 5, start = 0}) => ({
+                url: '/posts',
+                params: {
+                    _limit: limit,
+                    _start:start
+                },
+            }),
+        }),
+        fetchPostById: build.query<IPost, number>({
+            query: (id: number) => `/posts/${id}`,
+        }),
+    }),
+})
