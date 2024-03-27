dc91794bcd09d8ebe6f7ca1e9bae973e8968118f
helga-umrikh <zetaumr@gmail.com>
2024-03-27 16:19:05 +0300

    feat: add store Provider to root component

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,20 @@
-import React from 'react'
+import React from 'react';
+import './styles.css'
 import ReactDOM from 'react-dom/client'
-import './index.css'
 import App from './app/App'
-import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux'
+import { BrowserRouter as Router } from 'react-router-dom'
+import { setupStore } from './app/redux/store'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+const store = setupStore()
+
 root.render(
     <React.StrictMode>
-        <Router>
-            <App />
-        </Router>
+        <Provider store={store}>
+            <Router>
+                <App />
+            </Router>
+        </Provider>
     </React.StrictMode>
 )
