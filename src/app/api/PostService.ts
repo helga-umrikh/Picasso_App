d46d79c24f204cd9c6ddd5073a6f58cf9fcedf2b
helga-umrikh <zetaumr@gmail.com>
2024-03-27 16:10:58 +0300

    fix: add force refetch for postAPI

--- src/app/api/PostService.ts
+++ src/app/api/PostService.ts
@@ -1,24 +1,34 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { IPost } from '../interface/IPost'
 
-
 export const postAPI = createApi({
     reducerPath: 'postAPI',
     baseQuery: fetchBaseQuery({
         baseUrl: 'https://jsonplaceholder.typicode.com/',
     }),
-    endpoints: (build) => ({
-        fetchAllPosts: build.query<IPost[], {limit:number,start:number}>({
-            query: ({limit = 5, start = 0}) => ({
+    endpoints: (builder) => ({
+        fetchAllPosts: builder.query<IPost[], { limit: number, startCount: number}>({
+            query: ({limit, startCount}) => ({
                 url: '/posts',
                 params: {
                     _limit: limit,
-                    _start:start
+                    _start: startCount,
                 },
             }),
+            serializeQueryArgs: ({ endpointName }) => {
+                return endpointName;
+            },
+            merge: (currentCache, newItems) => {
+                currentCache.push(...newItems);
+            },
+            forceRefetch({ currentArg, previousArg }) {
+                return currentArg !== previousArg;
+            }
         }),
-        fetchPostById: build.query<IPost, number>({
+        fetchPostById: builder.query<IPost, number>({
             query: (id: number) => `/posts/${id}`,
         }),
     }),
 })
+
+export const { useFetchAllPostsQuery } = postAPI;
