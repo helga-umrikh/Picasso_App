40c8df453082f1b63c41b1c0ba808a13d0dd24d2
helga-umrikh <zetaumr@gmail.com>
2024-03-27 16:00:38 +0300

    feat: add header, wrap and styles for App

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,21 +1,22 @@
-import React from 'react'
+import React from 'react';
+import "./App.scss";
 import { Routes, Route } from 'react-router-dom'
 import HomePage from '../pages/HomePage/HomePage'
 import PostPage from '../pages/PostPage/PostPage'
-import {  Layout, Flex  } from 'antd';
-import HeaderComponent from '../widgets/ui/Header/HeaderComponent';
+import { Layout, Flex } from 'antd'
+import HeaderComponent from '../widgets/ui/Header/HeaderComponent'
 
 const layoutStyle = {
-  borderRadius: 8,
-  overflow: 'hidden',
-};
+    borderRadius: 8,
+    overflow: 'hidden',
+}
 
 function App() {
     return (
         <div className="App">
-            <Flex gap="middle" wrap="wrap">
+            <Flex gap="middle" wrap="wrap" className='wrap'>
                 <Layout style={layoutStyle}>
-                  <HeaderComponent />
+                    <HeaderComponent />
                     <Routes>
                         <Route path="/" element={<HomePage />} />
                         <Route path="/post/:id" element={<PostPage />} />
